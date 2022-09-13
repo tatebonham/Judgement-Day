e016d4e48e96b09b81fd1f62fe487c3e6012e61a
tatebonham <tatebonham@gmail.com>
2022-09-13 14:32:43 -0700

    added models association

--- models/post.js
+++ models/post.js
@@ -10,6 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      post.hasMany(comment)
+      post.belongsTo(user)
       // define association here
     }
   }
--- models/user.js
+++ models/user.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      user.hasMany(post)
       // define association here
     }
   }
